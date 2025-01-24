be5b4a9a4dfa351c7306fb06d5645d24815df444
EneAndrei789 <andreileonardene@gmail.com>
2025-01-24 17:41:04 +0200

    first commit!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import Profile from "./Profile/Profile";
+import Statistics from "./Statistics/Statistics";
+import user from './user.json';
+import data from './data.json'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <Profile user={user}/>
+      {/* <Statistics title="Upload stats" stats={data} /> */}
+<Statistics stats={data} />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,50 @@
+import styles from './Profile.module.css'
+import PropTypes, { shape } from 'prop-types'
+
+const Profile = ({user}) => {
+    const { avatar, username, tag, location, stats } = user;
+    return (
+        <div className={styles.profile}>
+          <div className={styles.avatar}>
+            <img src={avatar} alt={`${tag}'s avatar`} />
+          </div>
+          <div className={styles.userInfo}>
+            <h2 className={styles.name}>{username}</h2>
+            <p className={styles.username}>{username}</p>
+            <p className={styles.location}>{location}</p>
+          </div>
+          <div className={styles.stats}>
+            <div className={styles.stat}>
+              <span className={styles.label}>Followers</span>
+              <span className={styles.value}>{stats.followers}</span>
+            </div>
+            <div className={styles.stat}>
+              <span className={styles.label}>Views</span>
+              <span className={styles.value}>{stats.views}</span>
+            </div>
+            <div className={styles.stat}>
+              <span className={styles.label}>Likes</span>
+              <span className={styles.value}>{stats.likes}</span>
+            </div>
+          </div>
+        </div>
+      );
+};
+
+Profile.propTypes = {
+    user: PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      username: PropTypes.string.isRequired,
+      tag: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+      }).isRequired,
+    }).isRequired,
+  };
+
+  export default Profile;
+
+
